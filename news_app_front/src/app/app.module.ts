6b3af157de30338a6afa8f630989342a936287f3
amro-yasser <yasserdjar@gmail.com>
2023-11-18 00:36:36 +0400

    adding styles + fixing app module imports

--- news_app_front/src/app/app.module.ts
+++ news_app_front/src/app/app.module.ts
@@ -1,24 +1,40 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { DefaultComponent } from './layout/default/default.component';
 import { HeaderComponent } from './layout/header/header.component';
 import { FooterComponent } from './layout/footer/footer.component';
-
+import { HomeComponent } from './pages/home/home.component';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule} from '@angular/forms';
+import {MatSelectModule} from '@angular/material/select';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import { ArticleCardComponent } from './components/article-card/article-card.component';
 @NgModule({
   declarations: [
     AppComponent,
     DefaultComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    ArticleCardComponent
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
     AppRoutingModule,
-    NoopAnimationsModule
+    NoopAnimationsModule,
+    FormsModule, 
+    MatFormFieldModule, 
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
