e2f021fa1abdf4f24047f64762209698e4f32cb3
amro-yasser <yasserdjar@gmail.com>
2023-11-18 00:34:08 +0400

    create home component

---
+++ news_app_front/src/app/pages/home/home.component.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+import { NewsService } from 'src/app/services/news.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+  
+})
+export class HomeComponent {
+  languages: { label: string; code: string }[] = [
+      {label:'English',code:'en'},
+      { label:'Arabic', code:'ar'}
+    ]
+  
+  themes :string[] = [
+    'business',
+    'entertainment',
+    'general',
+    'health',
+    'science', 
+    'sports', 
+    'technology'
+  ]
+  query: string = ''  
+  selectedTheme: any = ''
+  selectedLanguage: any = '' 
+  articles: any[] = []
+
+  constructor(
+    private newsService: NewsService
+    ) {}
+
+  onsubmit(){
+
+    let params = {
+      q:this.query,
+      language:this.selectedLanguage
+    }
+    console.log(params)
+    this.newsService.retrieveObjects(params).subscribe((data:any)=>{
+      this.articles = data
+      console.log(this.articles)
+    })
+  }
+}
