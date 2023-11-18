d99265355f3b3ff61e37181173656cfccc984b15
amro-yasser <yasserdjar@gmail.com>
2023-11-19 02:54:18 +0400

    optimize search performance

--- news_app_backend/main/views.py
+++ news_app_backend/main/views.py
@@ -4,6 +4,9 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .utils import get_news, get_top_headlines
 from django.views import View
+from .models import Search
+import datetime
+import logging
 
 class NewsListView(ListAPIView):
      
@@ -17,17 +20,30 @@ class NewsListView(ListAPIView):
             language = self.request.GET.get("language", "en")
             page = int(self.request.GET.get("page", ""))
             
-            response = get_news(query, language, page)
-            articles = response.get("articles", [])
-            status = response.get("status", "")
+            today = datetime.date.today()
 
-            if status == 'ok' and articles:
-                return Response(response.get("articles", []))
+            search = Search.objects.filter(query=query, language=language, page=page)
+            if search.exists():
+                search = search.first()
+                if search.date != today:
+                    search.date = today
+                    search.save()
+
+                return Response(search.result)
+            else:
+                response = get_news(query=query,language=language,page=page)
+                articles = response.get("articles", [])
+                status = response.get("status", "")
+
+                if status == 'ok' and articles:
+                    Search.objects.create(query=query, language=language, page=page, result=articles, date=today)
+                    return Response(articles)
             
-            if not articles and status == 'ok':
-                return Response({"message": "Oops! It looks like we couldn't find any results."}, status=404)
+                if not articles and status == 'ok':
+                    return Response({"message": "Oops! It looks like we couldn't find any results."}, status=404)
+           
             
         except Exception as e:
-            
+            logging.error("An error occurred: %s", e, exc_info=True)
             return Response({"message": "Oops! It looks something went wrong."}, status=500)
     
