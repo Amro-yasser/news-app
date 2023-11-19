c034caa929b827013bcf662be895cb02109b3694
amro-yasser <yasserdjar@gmail.com>
2023-11-20 00:41:31 +0400

    fix cors allowed orignis

--- news_app_backend/app/settings.py
+++ news_app_backend/app/settings.py
@@ -75,7 +75,7 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:4200",
     "http://127.0.0.1:4200",
-    "http://127.0.0.1:4004",
+    "http://127.0.0.1:4000",
     "http://localhost:8000"
 ]
 
